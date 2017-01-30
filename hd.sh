2343e14ea3f8c8564ad0ad81b97e13a72ec40983
Ka Wong <kyw14@point52.doc.ic.ac.uk>
2017-01-30 17:21:05 +0000

    Hadoop standalone setup complete

--- hd.sh
+++ hd.sh
@@ -1,8 +1,10 @@
-sudo apt-get install default-jdk
-wget http://apache.mirror.anlx.net/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz
-tar xzf hadoop-2.6.5.tar.gz
+ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa 
+sudo apt-get -y install default-jdk
+wget http://apache.mirror.anlx.net/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz
+tar xzf hadoop-2.6.0.tar.gz
 mkdir hadoop
-mv hadoop-2.6.5/* to hadoop/
-rm -rf hadoop-2.6.5.tar.gz
+mv hadoop-2.6.0/* to hadoop/
+rm -rf hadoop-2.6.0.tar.gz
+rm -rf hadoop-2.6.0
 cd hadoop/etc/hadoop
-vim hadoop-env.sh
+sed -i '25 c\export JAVA_HOME=\/usr\/lib\/jvm\/java-8-openjdk-amd64' hadoop-env.sh
