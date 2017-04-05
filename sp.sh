0a83615d30bbc9c55cf62cbf73475849a3a7995d
Winnie <bijou886@gmail.com>
2017-04-05 22:34:06 +0800

    Delete sp.sh

--- sp.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install default-jdk
-sudo apt-get install scala
-wget http://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
-tar xvf spark-2.0.2-bin-hadoop2.7.tgz
-rm -rf spark-2.0.2-bin-hadoop2.7.tgz
-vim .bashsrc
