e9c5c7ca2c5c2ad351af59e6f2d3467c8783ad29
Winnie <bijou886@gmail.com>
2017-04-06 23:06:59 +0800

    ok

--- preallocate.sh
+++ preallocate.sh
@@ -2,6 +2,7 @@ cd
 cd flink-1.2.0/conf
 sed -i "57 c\taskmanager.memory.preallocate: $1" flink-conf.yaml
 cd ..
+bin/start-local.sh
 rm /home/kyw14/wordcount-result.txt
 bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
 rm /home/kyw14/wordcount-result.txt
@@ -12,3 +13,4 @@ rm /home/kyw14/wordcount-result.txt
 bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
 rm /home/kyw14/wordcount-result.txt
 bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
+bin/start-local.sh
