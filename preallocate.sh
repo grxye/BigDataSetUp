8265f89eeddb8626c8fb6c9105ef44a37f8c3080
Winnie <bijou886@gmail.com>
2017-04-06 17:19:24 +0800

    added code for running 5 times

--- preallocate.sh
+++ preallocate.sh
@@ -1,6 +1,14 @@
 cd
 cd flink-1.2.0/conf
-echo 'taskmanager.memory.preallocate = true' >> flink-conf.yaml
+sed -i '59 c\taskmanager.memory.preallocate: false' flink-conf.yaml
 cd ..
+rm /home/kyw14/wordcount-result.txt
+bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/1G.txt --output /home/kyw14/wordcount-result.txt
+rm /home/kyw14/wordcount-result.txt
+bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/1G.txt --output /home/kyw14/wordcount-result.txt
+rm /home/kyw14/wordcount-result.txt
+bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/1G.txt --output /home/kyw14/wordcount-result.txt
+rm /home/kyw14/wordcount-result.txt
+bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/1G.txt --output /home/kyw14/wordcount-result.txt
 rm /home/kyw14/wordcount-result.txt
 bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/1G.txt --output /home/kyw14/wordcount-result.txt
