a9cd92b273e116bdd03f7f9aada0b9338cfbaaca
Winnie <bijou886@gmail.com>
2017-04-06 20:57:20 +0800

    DED

--- offheap.sh
+++ offheap.sh
@@ -1,6 +1,6 @@
 cd
 cd flink-1.2.0/conf
-sed -i "61 c\taskmanager.memory.off-heap: $1" flink-conf.yaml
+sed -i "59 c\taskmanager.memory.off-heap: $1" flink-conf.yaml
 cd ..
 rm /home/kyw14/wordcount-result.txt
 bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
