eb4c412483c2ccc4aed64a721c771df147e18120
Winnie <bijou886@gmail.com>
2017-04-08 22:49:19 +0800

    Fixed minor bug in default

--- default.sh
+++ default.sh
@@ -1,12 +1,12 @@
 cd
 cd flink-1.2.0
 rm /home/kyw14/wordcount-result.txt
-bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
+bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$1.txt --output /home/kyw14/wordcount-result.txt
 rm /home/kyw14/wordcount-result.txt
-bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
+bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$1.txt --output /home/kyw14/wordcount-result.txt
 rm /home/kyw14/wordcount-result.txt
-bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
+bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$1.txt --output /home/kyw14/wordcount-result.txt
 rm /home/kyw14/wordcount-result.txt
-bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
+bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$1.txt --output /home/kyw14/wordcount-result.txt
 rm /home/kyw14/wordcount-result.txt
-bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
+bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$1.txt --output /home/kyw14/wordcount-result.txt
