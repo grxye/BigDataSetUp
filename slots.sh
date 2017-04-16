edc0729adaf0b75c1e4dc3762f818acd33fabb21
Winnie <bijou886@gmail.com>
2017-04-16 15:53:01 +0800

    Yes

--- slots.sh
+++ slots.sh
@@ -1,7 +1,9 @@
 cd
 cd flink-1.2.0/conf
+bin/stop-local.sh
 sed -i "52 c\taskmanager.numberOfTaskSlots: $1" flink-conf.yaml
 sed -i "61 c\parallelism.default: $1" flink-conf.yaml
+bin/start-local.sh
 cd ..
 rm /home/kyw14/wordcount-result.txt
 bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
