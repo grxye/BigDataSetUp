d7f9065cc35a8f95897435408a2044f3707222f8
Winnie <bijou886@gmail.com>
2017-04-16 15:54:36 +0800

    why:

--- slots.sh
+++ slots.sh
@@ -1,10 +1,10 @@
 cd
 cd flink-1.2.0/conf
-bin/stop-local.sh
 sed -i "52 c\taskmanager.numberOfTaskSlots: $1" flink-conf.yaml
 sed -i "61 c\parallelism.default: $1" flink-conf.yaml
-bin/start-local.sh
 cd ..
+bin/stop-local.sh
+bin/start-local.sh
 rm /home/kyw14/wordcount-result.txt
 bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
 rm /home/kyw14/wordcount-result.txt
