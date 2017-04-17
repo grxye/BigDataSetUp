7b200a3fe9ce92eaa181415bd81dcebe79d53800
Winnie <bijou886@gmail.com>
2017-04-17 23:19:15 +0800

    compiler sh file

--- maxlinesample.sh
+++ maxlinesample.sh
@@ -1,6 +1,6 @@
 cd
 cd flink-1.2.0/conf
-sed -i "57 c\taskmanager.memory.preallocate: $1" flink-conf.yaml
+sed -i "63 c\compiler.delimited-informat.max-line-samples: $1" flink-conf.yaml
 cd ..
 rm /home/kyw14/wordcount-result.txt
 bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
