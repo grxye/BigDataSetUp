0879c6ed96b8fae5333d75bf82fd16394a15766a
Winnie <bijou886@gmail.com>
2017-04-18 12:00:35 +0800

    Trying to reboot

--- maxlinesample.sh
+++ maxlinesample.sh
@@ -2,6 +2,8 @@ cd
 cd flink-1.2.0/conf
 sed -i "63 c\compiler.delimited-informat.max-line-samples: $1" flink-conf.yaml
 cd ..
+bin/stop-local.sh
+bin/start-local.sh
 rm /home/kyw14/wordcount-result.txt
 bin/flink run ./examples/batch/WordCount.jar --input /home/kyw14/$2.txt --output /home/kyw14/wordcount-result.txt
 rm /home/kyw14/wordcount-result.txt
