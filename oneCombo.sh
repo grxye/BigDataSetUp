cead91c11913f868f1d1d16ca85840779e9e48a5
Winnie <bijou886@gmail.com>
2017-05-04 18:10:43 +0100

    Update oneCombo.sh

--- oneCombo.sh
+++ oneCombo.sh
@@ -7,14 +7,15 @@ bin/stop-local.sh
 bin/start-local.sh
 rm /data/wordcount-result.txt
 bin/flink run ./examples/$2/WordCount.jar --input /home/kyw14/$1.txt --output /data/wordcount-result.txt >> /data/$5.txt
-rm /home/kyw14/wordcount-result.txt
+rm /data/wordcount-result.txt
 bin/flink run ./examples/$2/WordCount.jar --input /home/kyw14/$1.txt --output /data/wordcount-result.txt >> /data/$5.txt
-rm /home/kyw14/wordcount-result.txt
+rm /data/wordcount-result.txt
 bin/flink run ./examples/$2/WordCount.jar --input /home/kyw14/$1.txt --output /data/wordcount-result.txt >> /data/$5.txt
-rm /home/kyw14/wordcount-result.txt
+rm /data/wordcount-result.txt
 bin/flink run ./examples/$2/WordCount.jar --input /home/kyw14/$1.txt --output /data/wordcount-result.txt >> /data/$5.txt
-rm /home/kyw14/wordcount-result.txt
+rm /data/wordcount-result.txt
 bin/flink run ./examples/$2/WordCount.jar --input /home/kyw14/$1.txt --output /data/wordcount-result.txt >> /data/$5.txt
-echo $'$5 \n' >> /data/allResults.txt
-grep 'Job Runtime:' >> /data/allResults.txt
+echo "$5" >> /data/allResults.txt
+echo $'\n' >> /data/allResults.txt
+grep 'Job Runtime:' $5.txt >> /data/allResults.txt
 echo $'\n\n' >> /data/allResults.txt
